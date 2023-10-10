02db722a50abc20bec942efae93114cafdd827d4
Jacopo Cirica <jacopocirica@Jacopos-MacBook-Pro.local>
2023-10-10 16:36:30 -0700

    first commit

---
+++ client/script.js
@@ -0,0 +1,162 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+const previous_message=[{
+  "role": "system",
+  "content": "You are a helpful assistant who translates my instructions into code. You always provide me the code of an index.html file (with the css code in the style tag). Always write the Javascript component separately as if it were another file, by starting with ```javascript"
+}]
+
+let loadInterval
+
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+    let user=data.get('prompt').trim()
+    previous_message.push({
+      "role": "user",
+      "content":user})
+    
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5500', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'),
+            previous: previous_message
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        let output = document.getElementById("output");
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        previous_message.push({
+          "role": "assistant",
+          "content":parsedData})
+        console.log(previous_message)
+        let inputText=data.bot
+        let match = inputText.match(/```html([\s\S]*?)```/i); // Use regex to find html content
+        let match2 = inputText.match(/```javascript([\s\S]*?)```/); // Use regex to find html content
+        
+        let htmlContent;
+
+        if (match && match2) {  // If there's a match and match2
+          
+            
+            htmlContent = match[1] + "</html>";  // Add </html> to the extracted text because it was part of the delimiter
+            output.contentDocument.body.innerHTML = htmlContent.trim()
+            console.log(match2[1])
+            output.contentWindow.eval(match2[1])
+            console.log(htmlContent.trim()); // Print out the HTML content
+        
+        
+        }else if (match){
+          htmlContent = match[1];  // Add </html> to the extracted text because it was part of the delimiter
+            output.contentDocument.body.innerHTML = htmlContent.trim()
+            console.log(htmlContent.trim()); // Print out the HTML content
+        }
+         else {
+        console.log("No HTML content found");
+        }
+        
+        
+        
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,74 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import OpenAI from 'openai'
+
+dotenv.config()
+
+//const configuration = new Configuration({
+//  apiKey: process.env.OPENAI_API_KEY,
+//});
+
+//const openai = new OpenAIApi(configuration);
+
+const openai= new OpenAI({
+    apiKey: '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',
+});
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const previous_message = req.body.previous
+    console.log(prompt)
+    console.log(previous_message)
+
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: previous_message,
+        temperature: 1,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+    console.log(prompt)
+    //console.log(response.choices[0].message.content)
+    let inputText=response.choices[0].message.content
+
+    //let htmlContent = text.match(/```html([\s\S]*?)```/)[1];
+
+    //console.log(htmlContent.trim());   // This will print the HTML content
+
+    let match = inputText.match(/```html([\s\S]*?)```/); // Use regex to find html content
+    let htmlContent;
+
+    if (match) {  // If there's a match
+        htmlContent = match[1] + "</html>";  // Add </html> to the extracted text because it was part of the delimiter
+        console.log(htmlContent.trim()); // Print out the HTML content
+    } else {
+    console.log("No HTML content found");
+    }
+
+    res.status(200).send({
+      bot: response.choices[0].message.content
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5500, () => console.log('AI server started on http://localhost:5500'))
