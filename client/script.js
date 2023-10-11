65c5ae979be4f1dd5dc4eb91aa2ca8742cba6f6e
Jacopo Cirica <91603474+JacopoCirica@users.noreply.github.com>
2023-10-10 17:22:38 -0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5500', {
+    const response = await fetch('https://formatgpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
