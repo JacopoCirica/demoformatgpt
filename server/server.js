bb7e123c52ba03b135f47e8e4a647dbfb4fc61cf
Jacopo Cirica <91603474+JacopoCirica@users.noreply.github.com>
2023-10-19 11:43:32 -0600

    Update server.js 19oct

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.post('/', async (req, res) => {
         model: chatmodel,
         messages: previous_message,
         temperature: 0.5,
-        max_tokens: 2500,
+        max_tokens: 3500,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
